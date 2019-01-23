4dd04b9be9000ea005fb9c31eacc159d12b40aa6
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-23 14:48:37 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,22 +1,21 @@
 class CouponsController < ApplicationController
-
   def index
-    @coupons = Coupon.all
-  end
+     @coupons = Coupon.all
+   end
 
-  def new
-  end
+   def new
+   end
 
-  def create
-    # binding.pry
-    coupon = Coupon.new
-    coupon.coupon_code = params[:coupon][:coupon_code]
-    coupon[:store] = params[:coupon][:store]
-    coupon.save
-    redirect_to coupon_path(coupon)
-  end
+   def create
+     # binding.pry
+     coupon = Coupon.new
+     coupon.coupon_code = params[:coupon][:coupon_code]
+     coupon[:store] = params[:coupon][:store]
+     coupon.save
+     redirect_to coupon_path(coupon)
+   end
 
-  def show
-    @coupon = Coupon.find params[:id]
-  end
-end
+   def show
+     @coupon = Coupon.find params[:id]
+   end
+ end
