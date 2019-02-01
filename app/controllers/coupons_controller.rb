6356e9f57d1d314e62e54feb5b31f938e59ef9eb
Saint-Jean Germila <germila@noukod.com>
2019-02-01 17:37:48 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,28 +1,20 @@
 class CouponsController < ApplicationController
-before_action :set_coupon, only: :show
   def index
     @coupons = Coupon.all
   end
 
   def show
-    # @coupon = Coupon.find(params[:id])
+    @coupon = Coupon.find(params[:id])
   end
 
   def new
-   end
+  end
 
   def create
-    # session[:form_params] = params.inspect
-    # redirect_to new_coupon_path
     @coupon = Coupon.new
     @coupon[:coupon_code] = params[:coupon][:coupon_code]
     @coupon[:store] = params[:coupon][:store]
     @coupon.save
     redirect_to coupon_path(@coupon)
-  end
-  private
-
-  def set_coupon
-    @coupon = Coupon.find(params[:id])
   end
 end
